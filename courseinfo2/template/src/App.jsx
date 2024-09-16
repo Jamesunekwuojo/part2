36eeea465f22a671793a8e033db9692e5aef6755
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-09-16 18:14:07 +0100

    Done with courseInfo2, Course module extracted from app.jsx

--- courseinfo2/template/src/App.jsx
+++ courseinfo2/template/src/App.jsx
@@ -1,63 +1,7 @@
 
-
-
-
+import Course from './Course';
 
 const App = () => {
-
-
-const Course = () => {
-
-    const Header = ({course}) => <h1>{course}</h1>
-
-    const Content = ({parts}) => {
-
-      const Total = () => {
-        const initialTotal = 0
-
-
-
-        const sum = parts.reduce((s,p) => s + p.exercises, initialTotal,)
-
-        console.log(sum)
-
-        return (
-          <div>
-            <h3>total of {sum} exercise </h3>
-          </div>
-        )
-
-      }
-
-      //Part component
-      const Part = ({part}) =>
-        <p>
-          {part.name} {part.exercises}
-        </p>
-
-      return (
-        <div>
-          <Part part={parts[0]}></Part>
-          <Part part={parts[1]}></Part>
-          <Part part={parts[2]}></Part>
-          <Part part={parts[3]}></Part>
-          <Total></Total>
-
-
-        </div>
-      )
-    }
-    return (
-      <div>
-        <Header course={course}></Header>
-        <Content parts={parts}></Content>
-
-      </div>
-    )
-  }
-
- 
-
   const courses = [
     {
       name: 'Half Stack application development',
@@ -84,7 +28,7 @@ const Course = () => {
           id: 4
         }
       ]
-    }, 
+    },
     {
       name: 'Node.js',
       id: 2,
@@ -101,13 +45,15 @@ const Course = () => {
         }
       ]
     }
-  ]
+  ];
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map(course => (
+        <Course key={course.id} course={course} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
