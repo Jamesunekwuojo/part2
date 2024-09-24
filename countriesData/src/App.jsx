9a764d090dc6da7d2b781fe16b37faebd4557be4
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-09-24 22:03:51 +0100

    fetching Data successfully from REST API, flags and languages finally showing

--- countriesData/src/App.jsx
+++ countriesData/src/App.jsx
@@ -1,98 +1,79 @@
-import { useState, } from 'react'
+import { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 
-const CountryList = ({filteredCountries}) => {
-
-  if(filteredCountries > 10) {
+const CountryList = ({ filteredCountries }) => {
+  if (filteredCountries.length > 10) {
     return <p>Too many matches, specify another filter</p>;
   }
-  if (filteredCountries == 1) {
-    return filteredCountries.map(country =>{
-      return (
-        <div>
-          {country.name}
-          {country.capital}
-          {country.flags}
-        </div>
-      )
-    } )
-  
-    
+  if (filteredCountries.length === 1) {
+    const country = filteredCountries[0];
+    const languages = Object.entries(country.languages).map(([code, language]) => (
+      <li key={code}>{language}</li>
+    ))
+
+    return (
+      <div>
+        <h2>{country.name.common}</h2>
+        <p>Capital: {country.capital[0]}</p>
+        <p>area{country.area}</p>
+        <h3>Languages:</h3>
+        <ul>{languages}</ul>
+        <img className='countryFlag' src={country.flags.svg} alt={`Flag of ${country.name.common}`} />
+      </div>
+    );
   }
 
-  return(
-    <div>
-      {filteredCountries.map((country) => {
-        <li>
-          {country.name}
+  return (
+    <ul>
+      {filteredCountries.map((country) => (
+        <li key={country.cca2}>
+          {country.name.common}
         </li>
-      })}
-    </div>
-  )
-
-}
+      ))}
+    </ul>
+  );
+};
 
 function App() {
-
-  // const [countryName, setCountryName] = useState(null)
-  const [countries, setCountries] = useState('');
+  const [countries, setCountries] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
 
+  useEffect(() => {
+    const handleRequest = async () => {
+      const url = 'https://studies.cs.helsinki.fi/restcountries/api/all';
+      try {
+        const response = await axios.get(url);
+        setCountries(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    handleRequest();
+  }, []);
+
+  const filteredCountries = countries.filter((country) => {
+    return (
+      typeof country.name?.common === 'string' &&
+      country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  });
+
   const handleChange = (e) => {
-
-    setSearchTerm(e.target.value)
-
-  }
-
-
-
-  
-
-  const handleRequest = () => {
-    const url = 'https://studies.cs.helsinki.fi/restcountries/api/all'
-
-
-    // Get data from REST API
-    axios.get(url)
-    .then((response) => {
-      
-      console.log(response)
-
-      setCountries(response.data)
-       
-    
-    })
-    .catch((error) => {
-      console.log(error)
-
-    })
-
-
-    
-  
-
-  }
-
-  const filteredCountries = countries.filter((country) =>  country.name.toLowerCase().includes(searchTerm.toLowerCase()))
-
-
-
+    setSearchTerm(e.target.value);
+  };
 
   return (
     <>
-    <div className='searchDiv'>
-      <p>find countries</p>
-      <input value={searchTerm} onChange={handleChange} className='searchInput' type="text" />
-    </div>
+      <div className='searchDiv'>
+        <p>find countries</p>
+        <input value={searchTerm} onChange={handleChange} className='searchInput' type="text" />
+      </div>
 
-    {/* Countrylists */}
-    <CountryList filteredCountries={filteredCountries}></CountryList>
-
-  
+      {/* CountryLists */}
+      <CountryList filteredCountries={filteredCountries} />
     </>
-  )
- 
+  );
 }
 
-export default App
+export default App;
