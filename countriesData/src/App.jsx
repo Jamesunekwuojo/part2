199907a8e1aad2b3aeebba5bfd253df812acf07d
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-09-24 18:10:34 +0100

    working on countriesData structure nd API calls

--- countriesData/src/App.jsx
+++ countriesData/src/App.jsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, } from 'react'
+import './App.css';
+import axios from 'axios';
+
+const CountryList = ({filteredCountries}) => {
+
+  if(filteredCountries > 10) {
+    return <p>Too many matches, specify another filter</p>;
+  }
+  if (filteredCountries == 1) {
+    return filteredCountries.map(country =>{
+      return (
+        <div>
+          {country.name}
+          {country.capital}
+          {country.flags}
+        </div>
+      )
+    } )
+  
+    
+  }
+
+  return(
+    <div>
+      {filteredCountries.map((country) => {
+        <li>
+          {country.name}
+        </li>
+      })}
+    </div>
+  )
+
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // const [countryName, setCountryName] = useState(null)
+  const [countries, setCountries] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleChange = (e) => {
+
+    setSearchTerm(e.target.value)
+
+  }
+
+
+
+  
+
+  const handleRequest = () => {
+    const url = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+
+
+    // Get data from REST API
+    axios.get(url)
+    .then((response) => {
+      
+      console.log(response)
+
+      setCountries(response.data)
+       
+    
+    })
+    .catch((error) => {
+      console.log(error)
+
+    })
+
+
+    
+  
+
+  }
+
+  const filteredCountries = countries.filter((country) =>  country.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='searchDiv'>
+      <p>find countries</p>
+      <input value={searchTerm} onChange={handleChange} className='searchInput' type="text" />
+    </div>
+
+    {/* Countrylists */}
+    <CountryList filteredCountries={filteredCountries}></CountryList>
+
+  
     </>
   )
+ 
 }
 
 export default App
